95782e8630b8caa82cc646143a195af75d15d2bb
Toulo-hub <pcgroupe1@gmail.com>
2021-01-10 14:59:43 +0100

    Second commit

--- app/src/main/java/com/example/medtub2/form.java
+++ app/src/main/java/com/example/medtub2/form.java
@@ -1,14 +1,57 @@
 package com.example.medtub2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class form extends AppCompatActivity {
+public class form extends AppCompatActivity implements View.OnClickListener{
+
+    public CardView card1, card2, card3, card4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        card1 = (CardView) findViewById(R.id.cardsign2);
+        card2 = (CardView) findViewById(R.id.cardlog2);
+        card3 = (CardView) findViewById(R.id.cardexam2);
+        card4 = (CardView) findViewById(R.id.cardcontac2);
+
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.cardsign2 :
+                i = new Intent(this,sign2.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardlog2 :
+                i = new Intent(this,log2.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardexam2 :
+                i = new Intent(this,exam2.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardcontac2 :
+                i = new Intent(this,contac2.class);
+                startActivity(i);
+                break;
+        }
+
     }
 }
